2cbaa9a8d364f29894022248fd05292e60027d9c
RedMagic <lijnbelt13@gmail.com>
2024-05-02 13:20:28 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    java
+    kotlin("jvm") version "1.9.22"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+
+    id("xyz.jpenilla.run-paper") version "2.2.2"
+}
+
+group = "com.redmagic"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "spigotmc-repo"
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+    maven {
+        name = "undefinedapiRepo"
+        url = uri("https://repo.undefinedcreation.com/repo")
+    }
+    maven("https://repo.codemc.io/repository/maven-snapshots/")
+}
+
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("com.redmagic:UndefinedAPI:0.4.32")
+    implementation("net.wesjd:anvilgui:1.9.2-SNAPSHOT")
+}
+
+tasks {
+
+    shadowJar {
+        archiveFileName.set("UndefinedToggles-shadow.jar")
+    }
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+    runServer {
+        minecraftVersion("1.20.4")
+    }
+}
+
+kotlin{
+    jvmToolchain(17)
+}
